d2ccb7184f0b932092e1ab496a238562cc728782
emanuel-italo <emanuelitaloleal@hotmail.com>
2025-09-23 15:09:36 -0300

    Aula python clarify

---
+++ 02_variaveis.py
@@ -0,0 +1,24 @@
+""" Trabalhando com tipos e Variaveis"""
+
+nome = "emanuel"
+sobreNome = "italo"
+idade = 24
+altura = 1.89
+bermuda = False
+calca = True
+
+
+print(nome)
+print(nome + " " + sobreNome)
+print(nome + " " + sobreNome + " tem " + str(idade) + "anos" )
+
+print( idade + 2)
+
+textoVariasLinhas = '''
+uma linha
+outra linha
+mais uma
+'''
+
+print(textoVariasLinhas)
+
---
+++ exemplo3.py
@@ -0,0 +1,14 @@
+nomeCompleto = "Emanuel italo leal"
+inicio = 1
+fim = inicio + 2
+
+print(nomeCompleto [inicio:fim])
+
+nome = input("Digite o seu nome: ")
+sobreNome = input("Informe seu sobre nome: ")
+
+print(f"Seu nome é {nome} e seu sobrenome é {sobreNome}")
+
+
+valor01 = int(input("insira seu primeiro valor: "))
+valor02 = int(input("insira seu segundo valor: "))
